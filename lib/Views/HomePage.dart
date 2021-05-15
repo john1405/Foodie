caeb8bfa4a604ded2926f129d72504ca178929fe
john1405 <himanshuone00@gmail.com>
2021-05-15 23:08:03 +0530

    first

---
+++ lib/Views/HomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
